88965461abb59fffda8efecefb7079f347afde3b
Titov Nikita <allureshura@mail.ru>
2023-05-14 11:26:44 +0500

    fix save&load#2

--- src/main/java/gui/GameVisual/GameModel.java
+++ src/main/java/gui/GameVisual/GameModel.java
@@ -1,5 +1,6 @@
 package gui.GameVisual;
 
+import gui.Extends.Mathematic.Mathematic;
 import gui.GameVisual.Position.GamePositions;
 import gui.GameVisual.Position.RobotPositions;
 import gui.GameVisual.Position.TargetPositions;
@@ -21,10 +22,7 @@ public class GameModel extends Observable {
     private volatile double m_robotPositionX = 100;
     private volatile double m_robotPositionY = 100;
     private volatile double m_robotDirection = 0;
-
-    private volatile int m_targetPositionX = 150;
-    private volatile int m_targetPositionY = 100;
-
+    private final TargetPositions targetPositions = new TargetPositions(150, 100);
     private static final double maxVelocity = 0.1;
     private static final double maxAngularVelocity = 0.001;
     public GameModel(){
@@ -37,13 +35,13 @@ public class GameModel extends Observable {
                 notifyObservers(
                         new GamePositions(
                                 new RobotPositions(
-                                        round(m_robotPositionX),
-                                        round(m_robotPositionY),
+                                        (int) Math.round(m_robotPositionX),
+                                        (int) Math.round(m_robotPositionY),
                                         m_robotDirection
                                 ),
                                 new TargetPositions(
-                                        m_targetPositionX,
-                                        m_targetPositionY
+                                        targetPositions.getX(),
+                                        targetPositions.getY()
                                 )
                         )
                 );
@@ -61,60 +59,13 @@ public class GameModel extends Observable {
 
     protected void setTargetPosition(Point p)
     {
-        m_targetPositionX = p.x;
-        m_targetPositionY = p.y;
+        targetPositions.setX(p.x);
+        targetPositions.setY(p.y);
     }
 
-    private static double distance(double x1, double y1, double x2, double y2)
-    {
-        double diffX = x1 - x2;
-        double diffY = y1 - y2;
-        return Math.sqrt(diffX * diffX + diffY * diffY);
-    }
 
-    private static double angleTo(double fromX, double fromY, double toX, double toY)
-    {
-        double diffX = toX - fromX;
-        double diffY = toY - fromY;
+    private double calculateAngularVelocity(double angleToTarget){
 
-        return asNormalizedRadians(Math.atan2(diffY, diffX));
-    }
-    private static double asNormalizedRadians(double angle)
-    {
-        while (angle < 0)
-        {
-            angle += 2*Math.PI;
-        }
-        while (angle >= 2*Math.PI)
-        {
-            angle -= 2*Math.PI;
-        }
-        return angle;
-    }
-    private static int round(double value)
-    {
-        return (int)(value + 0.5);
-    }
-
-    private static double applyLimits(double value, double min, double max)
-    {
-        if (value < min)
-            return min;
-        if (value > max)
-            return max;
-        return value;
-    }
-
-    protected void onModelUpdateEvent()
-    {
-        double distance = distance(m_targetPositionX, m_targetPositionY,
-                m_robotPositionX, m_robotPositionY);
-        if (distance < 0.5)
-        {
-            return;
-        }
-        double velocity = maxVelocity;
-        double angleToTarget = angleTo(m_robotPositionX, m_robotPositionY, m_targetPositionX, m_targetPositionY);
         double angularVelocity = 0;
 
         if (angleToTarget - m_robotDirection > Math.PI){
@@ -124,24 +75,41 @@ public class GameModel extends Observable {
         if (angleToTarget - m_robotDirection < -Math.PI){
             angularVelocity = maxAngularVelocity;
         }
-        if (angleToTarget - m_robotDirection < Math.PI && angleToTarget - m_robotDirection >=0){
+
+        if (angleToTarget - m_robotDirection < Math.PI && angleToTarget - m_robotDirection >=0)
+        {
             angularVelocity = maxAngularVelocity;
         }
 
         if (angleToTarget - m_robotDirection < 0 && angleToTarget - m_robotDirection >=-Math.PI){
             angularVelocity = -maxAngularVelocity;
         }
+
+        return angularVelocity;
+    }
+
+    protected void onModelUpdateEvent()
+    {
+        double distance = Mathematic.distance(targetPositions.getX(), targetPositions.getY(),
+                m_robotPositionX, m_robotPositionY);
+        if (distance < 0.5)
+        {
+            return;
+        }
+        double velocity = maxVelocity;
+        double angleToTarget = Mathematic.angleTo(m_robotPositionX, m_robotPositionY, targetPositions.getX(), targetPositions.getY());
+        double angularVelocity = calculateAngularVelocity(angleToTarget);
+
         if (unreachable()) {
             angularVelocity = 0;
-
         }
         moveRobot(velocity, angularVelocity, 10);
     }
 
     private void moveRobot(double velocity, double angularVelocity, double duration)
     {
-        velocity = applyLimits(velocity, 0, maxVelocity);
-        angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+        velocity = Mathematic.applyLimits(velocity, 0, maxVelocity);
+        angularVelocity = Mathematic.applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
 
 
         double newX = m_robotPositionX + (velocity / angularVelocity) *
@@ -165,14 +133,13 @@ public class GameModel extends Observable {
 
         m_robotPositionX = newX;
         m_robotPositionY = newY;
-        double newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
-
+        double newDirection = Mathematic.asNormalizedRadians(m_robotDirection + angularVelocity * duration);
 
         m_robotDirection = newDirection;
     }
     private boolean unreachable(){
-        double dx = m_targetPositionX - m_robotPositionX;
-        double dy = m_targetPositionY - m_robotPositionY;
+        double dx = targetPositions.getX() - m_robotPositionX;
+        double dy = targetPositions.getY() - m_robotPositionY;
 
         double new_dx = Math.cos(m_robotDirection)*dx + Math.sin(m_robotDirection)*dy;
         double new_dy = Math.cos(m_robotDirection)*dy - Math.sin(m_robotDirection)*dx;
@@ -181,9 +148,7 @@ public class GameModel extends Observable {
         double dist1 = (Math.sqrt(Math.pow((new_dx),2)+Math.pow(new_dy-y_center,2)));
         double dist2 = (Math.sqrt(Math.pow((new_dx),2)+Math.pow(new_dy+y_center,2)));
 
-        if (dist1 > maxVelocity/maxAngularVelocity && dist2 > maxVelocity / maxAngularVelocity)
-            return false;
-        return true;
+        return !(dist1 > maxVelocity / maxAngularVelocity) || !(dist2 > maxVelocity / maxAngularVelocity);
     }
 
 }
